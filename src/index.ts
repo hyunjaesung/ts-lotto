2de0ac9e1abe82ea4a1fb807f9dba0cc5cd9c097
Steve Sung <stevysung@gmail.com>
2024-04-28 09:10:18 +0900

    feat: Lotto 상금 계산

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ const generateUnduplicatedNumbers = ({
 };
 
 class LottoNumberGenerator {
-  static generate() {
+  static generate(): LottoNumber[] {
     return generateUnduplicatedNumbers({
       numbersLength: LOTTO_NUMBERS_LENGTH,
       maxNumber: LOTTO_MAX_NUMBER,
@@ -60,13 +60,131 @@ class LottoTicketGenerator {
   }
 }
 
+enum PRIZE_NAME {
+  FIRST = "FIRST",
+  SECOND = "SECOND",
+  THIRD = "THIRD",
+  FOURTH = "FOURTH",
+  FAIL = "FAIL",
+}
+
+enum PRIZE_MONEY {
+  FIRST = 2_000_000_000,
+  SECOND = 1_500_000,
+  THIRD = 50_000,
+  FOURTH = 5_000,
+  FAIL = 0,
+}
+
+class LottoResult {
+  private _result = {
+    [PRIZE_NAME.FIRST]: 0,
+    [PRIZE_NAME.SECOND]: 0,
+    [PRIZE_NAME.THIRD]: 0,
+    [PRIZE_NAME.FOURTH]: 0,
+    [PRIZE_NAME.FAIL]: 0,
+  };
+
+  setResult({ matchCount }: { matchCount: number }) {
+    switch (matchCount) {
+      case 6:
+        this._result[PRIZE_NAME.FIRST]++;
+        break;
+      case 5:
+        this._result[PRIZE_NAME.SECOND]++;
+        break;
+      case 4:
+        this._result[PRIZE_NAME.THIRD]++;
+        break;
+      case 3:
+        this._result[PRIZE_NAME.FOURTH]++;
+        break;
+      default:
+        this._result[PRIZE_NAME.FAIL]++;
+        break;
+    }
+  }
+
+  get result() {
+    return this._result;
+  }
+
+  getRateOfReturn() {
+    const totalPrizeMoney =
+      this._result[PRIZE_NAME.FIRST] * PRIZE_MONEY.FIRST +
+      this._result[PRIZE_NAME.SECOND] * PRIZE_MONEY.SECOND +
+      this._result[PRIZE_NAME.THIRD] * PRIZE_MONEY.THIRD +
+      this._result[PRIZE_NAME.FOURTH] * PRIZE_MONEY.FOURTH;
+
+    const ticketCount = Object.values(this._result).reduce(
+      (acc, cur) => acc + cur
+    );
+
+    return totalPrizeMoney / (LOTTO_PRICE * ticketCount);
+  }
+}
+
+class LottoResultCalculator {
+  lottoTickets: LottoTicket[];
+  winningNumbers: LottoNumber[];
+
+  constructor({
+    lottoTickets,
+    winningNumbers,
+  }: {
+    lottoTickets: LottoTicket[];
+    winningNumbers: LottoNumber[];
+  }) {
+    this.lottoTickets = lottoTickets;
+    this.winningNumbers = winningNumbers;
+  }
+
+  calculateResult() {
+    const result = new LottoResult();
+
+    this.lottoTickets.forEach(ticket => {
+      const matchCount = this.getMatchCount(ticket);
+      result.setResult({ matchCount });
+    });
+
+    return result;
+  }
+
+  private getMatchCount(ticket: LottoTicket) {
+    return ticket.lottoNumbers.filter(number =>
+      this.winningNumbers.includes(number)
+    ).length;
+  }
+}
+
 class LottoGame {
   readonly lottoTickets: LottoTicket[];
+
   constructor({ totalPurchase }: { totalPurchase: number }) {
     this.lottoTickets = LottoTicketGenerator.generate(totalPurchase);
+  }
+
+  getLottoResult(winningNumbers: number[]) {
+    // TODO 예외처리
+    // 1. 중복된 숫자가 있는지
+    // 2. 1~45 사이의 숫자인지
+    // 3. 6개의 숫자인지
+
+    const winningLottoNumbers = [...winningNumbers].map(
+      number => new LottoNumber(number)
+    );
+    const lottoResultCalculator = new LottoResultCalculator({
+      lottoTickets: this.lottoTickets,
+      winningNumbers: winningLottoNumbers,
+    });
+
+    return lottoResultCalculator.calculateResult();
   }
 }
 
 const lottoGame = new LottoGame({ totalPurchase: 3_000 });
 
-console.log(lottoGame.lottoTickets);
+console.log(
+  lottoGame.lottoTickets,
+  lottoGame.getLottoResult([1, 2, 3, 4, 5, 6])
+);
